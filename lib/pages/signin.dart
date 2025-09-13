0cde35b952365a62aae6fb9f052e38f9884002dd
nesrinedroul <ilhemnesrinedrl@gmail.com>
2025-09-13 21:41:09 +0100

    sign in

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -7,48 +7,7 @@ class Signin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      drawer: Container(
-        width: 200,
-        color: Colors.grey[200],
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.deepPurple,
-              ),
-              child: Text(
-                'Menu',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Home'),
-              onTap: () {
-                // Handle Home tap
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.person),
-              title: Text('Profile'),
-              onTap: () {
-                // Handle Profile tap
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
-              onTap: () {
-                // Handle Settings tap
-              },
-            ),
-          ],
-        ),
-      ),
+     
       appBar: AppBar(centerTitle: true,
         title: Text('Sign In'),
       ),
@@ -96,6 +55,13 @@ class Signin extends StatelessWidget {
                 
               ),
               SizedBox(height: 20),
+              ElevatedButton.icon(
+                onPressed: () {
+                  // Handle Google sign in
+                },
+                label: Text('Sign in with Google'),
+                icon: Icon(Icons.login),
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
